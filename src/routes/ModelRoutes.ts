ee2fdab2937906b9b7a72dc10a6a29301efda236
Arpad Magosanyi <m4gw4s@gmail.com>
2023-04-29 09:50:58 +0200

    build

--- src/routes/ModelRoutes.ts
+++ src/routes/ModelRoutes.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { CommonRoutesConfig } from "./CommonRoutesConfig.js";
-import { bound } from "cdd-ts";
 import type express from "express";
 import { GetSelbriControllerService } from "../controllers/GetSelbriControllerService.js";
 import { SaveSelbriControllerService } from "../controllers/SaveSelbriControllerService.js";
-
+import { BoundService } from "cdd-ts/src/util/BoundService.js";
 const MODEL = "model";
+
+const bound = BoundService.prototype.bound;
 
 export class UsersRoutes extends CommonRoutesConfig {
   constructor(app: express.Application) {
